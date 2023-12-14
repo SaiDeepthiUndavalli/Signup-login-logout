abd86adc393846e9652e328e11031a4e8fb782ee
Sai Deepthi Undavalli <112083457+SaiDeepthiUndavalli@users.noreply.github.com>
2023-12-14 20:28:42 +0530

    Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./Home.css"
+
+import { useNavigate } from 'react-router-dom'
 const Home = ({setLoginUser}) => {
+  const navigate=useNavigate()
   return (
+    
     <div className="homepage">
-       
-        <div className='button' onClick={()=>setLoginUser({})}>Logout</div>
+       <h1>Hello, Welcome to our page!</h1>
+        <div className='button' onClick={()=>navigate('/login')}>Logout</div>
     </div>
   )
 }
